e2693e36a9fe54e5c67df102a9f7e958b0f085ff
serjCookies <locobu@icloud.com>
2022-10-19 17:14:13 +0400

    printing the headers and their positions

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,4 +4,5 @@ filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    for index, columb_header in enumerate(header_row):
+        print(index, columb_header)
