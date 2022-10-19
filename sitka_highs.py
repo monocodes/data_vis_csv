6e042813039bf0c82b470e7c7605dfea9381da82
serjCookies <locobu@icloud.com>
2022-10-19 18:08:24 +0400

    added printing the headers and their position (enumerate function) to sitka_highs.py

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,10 +5,16 @@ filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
 
-    # 
+    # get the first line of the file
     header_row = next(reader)
-    
-    # or list comprehension
+
+    # printing the headers and their positions
+    """
+    for index, columb_header in enumerate(header_row):
+        print(index, columb_header)
+    """
+
+    # list comprehension
     #highs = [int(row[5]) for row in reader]
     highs = []
     for row in reader:
