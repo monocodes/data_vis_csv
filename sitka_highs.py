29b03b54c932ddbd9390f613c0dc02147dbdd769
serjCookies <locobu@icloud.com>
2022-10-19 18:02:51 +0400

    extracting and reading data and added useful comments

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,8 +1,18 @@
+from asyncore import read
 import csv
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
+
+    # 
     header_row = next(reader)
-    for index, columb_header in enumerate(header_row):
-        print(index, columb_header)
+    
+    # or list comprehension
+    #highs = [int(row[5]) for row in reader]
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+    
+print(highs)
