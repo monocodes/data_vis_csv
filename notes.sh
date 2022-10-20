625e88ddf218b7e846dbecf8b19f664bb6a679b1
serjCookies <locobu@icloud.com>
2022-10-20 12:56:49 +0400

    the datetime module

---
+++ notes.sh
@@ -0,0 +1,37 @@
+### The datetime Module ###
+
+Argument
+Meaning
+
+%A
+Weekday name, such as Monday
+
+%B
+Month name, such as January
+
+%m
+Month, as a number (01 to 12)
+
+%d
+Day of the month, as a number (01 to 31)
+
+%Y
+Four-digit year, such as 2019
+
+%y
+Two-digit year, such as 19
+
+%H
+Hour, in 24-hour format (00 to 23)
+
+%I
+Hour, in 12-hour format (01 to 12)
+
+%p
+AM or PM
+
+%M
+Minutes (00 to 59)
+
+%S
+Seconds (00 to 61)
