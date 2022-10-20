9dc3559a63b36d6cfe2d1bc7bf19c01b5d0779d3
serjCookies <locobu@icloud.com>
2022-10-20 14:21:54 +0400

    plotting a longer timeframe, switched to a different main script - sitka_highs_year.py

---
+++ sitka_highs_year.py
@@ -0,0 +1,42 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = 'data/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+
+    # get the first line of the file
+    header_row = next(reader)
+
+    # printing the headers and their positions
+    """
+    for index, columb_header in enumerate(header_row):
+        print(index, columb_header)
+    """    
+
+    # list comprehension
+    #highs = [int(row[5]) for row in reader]
+
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots(figsize=(13, 7))
+ax.plot(dates, highs, c='red')
+
+# Format plot.
+ax.set_title("Daily high temperatures, 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
